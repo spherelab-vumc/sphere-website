748253ddc66eefc6b18e619c887f890e615f646e
Vaidya <mihir.vaidya@vumc.org>
2025-05-29 12:03:02 -0500

    Improved the Navbar

--- frontend/src/components/Navbar/Navbar.tsx
+++ frontend/src/components/Navbar/Navbar.tsx
@@ -1,24 +1,25 @@
 import './Navbar.scss';
 // import vumcLogo from '../../../public/Vumc-logo.jpg';
 
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
 
 function Navbar() {
   return (
     <div className='custom-navbar'>
         {/* Left Side of the navebar */}
-        <div className='navbar-left'>
+        <Link to='/' className='navbar-left'>
             <img src='/Vanderbilt.webp' alt="VUMC Logo" className='logo' />
             <span className='lab-name'>SPHERE Lab</span>
-        </div>
+        </Link>
 
         {/* Right Side of the navbar */}
         <div className='navbar-right'>
-            <a href="#research">Research</a>
-            <a href="#news">News</a>
-            <a href="#features">Featured</a>
-            <a href="#about">About Us</a>
-            <a href="#publications">Publications</a>
+            <a href="research">Research</a>
+            <a href="news">News</a>
+            <a href="featured">Featured</a>
+            <a href="about-us">About Us</a>
+            <a href="publications">Publications</a>
         </div>
     </div>
   )
